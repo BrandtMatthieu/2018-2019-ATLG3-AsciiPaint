57d2a17d128b3897f1e4ad71883759e9a4d2cb11
44422 <44422@etu.he2b.be>
2018-10-07 14:59:24 +0200

    Changed integer to double + added hasShapeWithId method + added getShapeWithId method + added getIndexOfShapeWithId method + added listShapes method + added listHistory method + added addShape method + added removeShapeWithId method + added addToHistory mehtod + added render mathod + added clearShapes mathod + added moveShapeWithId method

--- src/esi/atl/g44422/model/Canvas.java
+++ src/esi/atl/g44422/model/Canvas.java
@@ -1,46 +1,46 @@
 package esi.atl.g44422.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Canvas {
-	private int sizeX;
-	private int sizeY;
+	private double sizeX;
+	private double sizeY;
 	private char background;
 	private boolean grid;
 	private boolean border;
 	private int id;
-	private Pixel[][] canvas;
 	private ArrayList<Shape> shapes;
 	private int shapeIdCount;
 	private ArrayList<String> history;
 
-	public Canvas(int sizeX, int sizeY, char background, boolean grid, boolean border, int id) throws IllegalArgumentException {
+	public Canvas(double sizeX, double sizeY, char background, boolean grid, boolean border, int id) throws IllegalArgumentException {
 		this.setSizeX(sizeY);
 		this.setSizeY(sizeY);
 		this.setBackground(background);
 		this.setGrid(grid);
 		this.setBorder(border);
 		this.setId(id);
-		this.setCanvas(sizeX, sizeY, background);
 	}
 
-	public int getSizeX() {
+	public double getSizeX() {
 		return sizeX;
 	}
 
-	public void setSizeX(int sizeX) throws IllegalArgumentException {
-		if(sizeX < 0) {
+	public void setSizeX(double sizeX) throws IllegalArgumentException {
+		if (sizeX < 0) {
 			throw new IllegalArgumentException();
 		}
 		this.sizeX = sizeX;
 	}
 
-	public int getSizeY() {
+	public double getSizeY() {
 		return sizeY;
 	}
 
-	public void setSizeY(int sizeY) throws IllegalArgumentException {
-		if(sizeY < 0) {
+	public void setSizeY(double sizeY) throws IllegalArgumentException {
+		if (sizeY < 0) {
 			throw new IllegalArgumentException();
 		}
 		this.sizeY = sizeY;
@@ -59,7 +59,7 @@ public class Canvas {
 	}
 
 	public void setId(int id) throws IllegalArgumentException {
-		if(id < 0 || id > this.getShapeIdCount()) {
+		if (id < 0) {
 			throw new IllegalArgumentException();
 		}
 		this.id = id;
@@ -81,19 +81,6 @@ public class Canvas {
 		this.border = border;
 	}
 
-	public Pixel[][] getCanvas() {
-		return canvas;
-	}
-
-	public void setCanvas(int sizeX, int sizeY, char background) {
-		this.canvas = new Pixel[sizeX][sizeY];
-		for(Pixel[] line : this.canvas) {
-			for(Pixel pixel : line) {
-				pixel = new Pixel(background);
-			}
-		}
-	}
-
 	public ArrayList<Shape> getShapes() {
 		return shapes;
 	}
@@ -107,7 +94,7 @@ public class Canvas {
 	}
 
 	public void setShapeIdCount(int shapeIdCount) throws IllegalArgumentException {
-		if(shapeIdCount < 0) {
+		if (shapeIdCount < 0) {
 			throw new IllegalArgumentException();
 		}
 		this.shapeIdCount = shapeIdCount;
@@ -124,5 +111,165 @@ public class Canvas {
 	@Override
 	public String toString() {
 		return "Canvas | Id: " + this.getId() + " Shape count: " + this.getShapes().size() + " Size X: " + this.getSizeX() + " Size Y: " + this.getSizeY() + " Background: " + this.getBackground();
+	}
+
+	public boolean hasShapeWithId(int id) {
+		for (Shape shape : this.getShapes()) {
+			if (shape.getId() == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Shape getShapeWithId(int id) throws IllegalArgumentException {
+		if (hasShapeWithId(id)) {
+			for (Shape shape : this.getShapes()) {
+				if (shape.getId() == id) {
+					return shape;
+				}
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public int getIndexOfShapeWithId(int id) throws IllegalArgumentException {
+		if (hasShapeWithId(id)) {
+			for (int i = 0; i < this.getShapes().size(); i++) {
+				if (this.getShapes().get(i).getId() == id) {
+					return i;
+				}
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public String listShapes() {
+		String str;
+		if (this.getShapes().size() == 0) {
+			str =  "There are no shapes in this canvas";
+		} else {
+			str = "List of shapes for canvas id: " + this.getId();
+			for (Shape shape : this.getShapes()) {
+				str += "\n\t" + shape.toString();
+			}
+		}
+		return str;
+	}
+
+	public String listHistory() {
+		String str;
+		if(this.getHistory().size() == 0) {
+			str = "No history for this canvas.";
+		} else {
+			str = "History for canvas id: " + this.getId();
+			for(String history : this.getHistory()) {
+				str += "\n\t" + history.toString();
+			}
+		}
+		return str;
+	}
+
+	public void addShape(String[] args) throws IllegalArgumentException {
+		switch (args[1].toLowerCase()) {
+		case "ellipse":
+			break;
+		case "circle":
+			break;
+		case "rectangle":
+			break;
+		case "square":
+			break;
+		case "line":
+			break;
+		case "point":
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public void removeShapeWithId(int id) throws IllegalArgumentException {
+		if (this.hasShapeWithId(id)) {
+			this.shapes.remove(this.getShapeWithId(id));
+			return;
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public void addToHistory(String args) {
+		this.history.add(String.join(" ", args));
+	}
+
+	public ArrayList<ArrayList<Pixel>> render() {
+		ArrayList<ArrayList<Pixel>> canvas = new ArrayList<ArrayList<Pixel>>();
+		for (int j = 1; j < this.getSizeY() + 1; j++) {
+			canvas.add(new ArrayList<Pixel>());
+			for (int i = 1; i < this.getSizeX() + 1; i++) {
+				canvas.get(j - 1).add(new Pixel(this.getBackground()));
+				for (Shape shape : this.getShapes()) {
+					if (shape.isInside(new Point(i, j))) {
+						canvas.get(j).get(i).setValue(shape.getFill());
+					}
+				}
+			}
+		}
+		return canvas;
+	}
+
+	public void clearShapes() {
+		this.setShapes(new ArrayList<Shape>());
+	}
+
+	public void moveShapeWithId(double dx, double dy, int id) throws IllegalArgumentException {
+		if (hasShapeWithId(id)) {
+			this.getShapeWithId(id).move(dx, dy);
+			return;
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public void moveShapeWithIdUp(int id) throws IllegalArgumentException {
+		if (hasShapeWithId(id)) {
+			if (id < this.getShapes().size()) {
+				Collections.swap(this.shapes, id, id + 1);
+				return;
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public void moveShapeWithIdDown(int id) throws IllegalArgumentException {
+		if (hasShapeWithId(id)) {
+			if (id > 0) {
+				Collections.swap(this.shapes, id, id - 1);
+				return;
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public void moveShapeWithIdFront(int id) throws IllegalArgumentException {
+		if (hasShapeWithId(id)) {
+			if (id < this.getShapes().size()) {
+				Shape temp = getShapeWithId(id);
+				this.shapes.remove(getShapeWithId(id));
+				this.shapes.add(temp);
+				return;
+			}
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public void moveShapeWithIdBack(int id) throws IllegalArgumentException {
+		if (hasShapeWithId(id)) {
+			if (id > 0) {
+				Shape temp = getShapeWithId(id);
+				this.shapes.remove(getShapeWithId(id));
+				this.shapes.add(1, temp);
+				return;
+			}
+		}
+		throw new IllegalArgumentException();
 	}
 }

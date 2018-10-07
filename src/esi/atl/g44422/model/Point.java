e4a0e55faf36a683666d7075f0e840b86ff85042
44422 <44422@etu.he2b.be>
2018-10-07 15:01:07 +0200

    added isInside mathod + added move method

--- src/esi/atl/g44422/model/Point.java
+++ src/esi/atl/g44422/model/Point.java
@@ -1,46 +1,52 @@
 package esi.atl.g44422.model;
 
 public class Point extends Shape {
-	private int x;
-	private int y;
+	private double x;
+	private double y;
 
-	public Point(int x, int y, Point center, ShapeType type, char fill, char outline, int id) {
+	public Point(double x, double y, Point center, ShapeType type, char fill, char outline, int id) {
 		super(center, type, fill, outline, id);
 	}
 
-	public Point(int x, int y) {
+	public Point(double x, double y) {
 		super();
 		this.setX(x);
 		this.setY(y);
 	}
 
-	public int getX() {
+	public double getX() {
 		return x;
 	}
 
-	public void setX(int x) {
+	public void setX(double x) {
 		this.x = x;
 	}
 
-	public int getY() {
+	public double getY() {
 		return y;
 	}
 
-	public void setY(int y) {
+	public void setY(double y) {
 		this.y = y;
-	}
-
-	public void move(int dx, int dy) {
-		this.setX(this.getX() + dx);
-		this.setY(this.getY() + dx);
 	}
 
 	@Override
 	public String toString() {
-		if(this instanceof Shape) {
+		if(this.getCenter() != null) { //shape
 			return super.toString();
-		} else {
+		} else { //center
 			return "(" + this.getX() + ", " + this.getY() + ")";
 		}
+	}
+
+	@Override
+	public boolean isInside(Point point) {
+		return point.getX() - this.getCenter().getX() <= 0.5 && point.getY() - this.getCenter().getY() <= 0.5;
+	}
+
+	@Override
+	public void move(double dx, double dy) {
+		this.setX(this.getX() + dx);
+		this.setY(this.getY() + dy);
 	}
 }

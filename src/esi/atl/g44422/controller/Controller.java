43b36dd5b8f7ea735745c16980c2840b06887ac1
44422 <44422@etu.he2b.be>
2018-10-07 15:04:45 +0200

    added some controler methods

--- src/esi/atl/g44422/controller/Controller.java
+++ src/esi/atl/g44422/controller/Controller.java
@@ -1,10 +1,58 @@
 package esi.atl.g44422.controller;
 
+import esi.atl.g44422.model.Viewport;
 import esi.atl.g44422.view.View;
 
 public class Controller {
-	View view;
-	public Controller(View view) {
+	private View view;
+	private Viewport viewport;
+
+	public Controller() {
+		this.setView(new View());
+		this.setViewport(new Viewport());
+	}
+
+	public View getView() {
+		return view;
+	}
+
+	public void setView(View view) {
 		this.view = view;
+	}
+
+	public Viewport getViewport() {
+		return viewport;
+	}
+
+	public void setViewport(Viewport viewport) {
+		this.viewport = viewport;
+	}
+
+	public void start() {
+		this.getView().displayLogo();
+		this.getView().displayWelcome();
+		while (true) {
+			this.getView().displayEnter();
+			String command = this.getView().getSc().nextLine();
+			this.handleCommand(command.split(" "));
+		}
+	}
+
+	public void handleCommand(String[] args) {
+		if (hasGoodNumberOfArguments(0, 10, args)) {
+			switch (args[0].toLowerCase()) {
+				case "quit":
+				case "exit":
+					System.exit(0);
+					break;
+				case "":
+					break;
+			}
+		}
+	}
+
+	public boolean hasGoodNumberOfArguments(int min, int max, String[] args) {
+		return (args.length - 1) >= min
+				&& (args.length - 1) <= max;
 	}
 }

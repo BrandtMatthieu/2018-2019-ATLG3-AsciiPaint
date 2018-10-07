8e79f2a7bff01113197f626785225d133f8959a3
44422 <44422@etu.he2b.be>
2018-10-07 15:00:02 +0200

    added isInside method + changed integer to double

--- src/esi/atl/g44422/model/Ellipse.java
+++ src/esi/atl/g44422/model/Ellipse.java
@@ -1,39 +1,39 @@
 package esi.atl.g44422.model;
 
-public class Ellipse extends Shape{
-	private int radiusX;
-	private int radiusY;
+public class Ellipse extends Shape {
+	private double radiusX;
+	private double radiusY;
 
-	public Ellipse(int radiusX, int radiusY, Point center, ShapeType type, char fill, char outline, int id) throws IllegalArgumentException {
+	public Ellipse(double radiusX, double radiusY, Point center, ShapeType type, char fill, char outline, int id) throws IllegalArgumentException {
 		super(center, null, fill, outline, id);
-		if(type == ShapeType.Ellipse) {
+		if (type == ShapeType.Ellipse) {
 			super.setType(ShapeType.Ellipse);
 			this.setRadiusX(radiusX);
 			this.setRadiusY(radiusY);
-		} else if(type == ShapeType.Circle) {
+		} else if (type == ShapeType.Circle) {
 			super.setType(ShapeType.Circle);
 			this.setRadiusX(radiusX);
 			this.setRadiusY(radiusY);
 		}
 	}
 
-	public int getRadiusX() {
+	public double getRadiusX() {
 		return radiusX;
 	}
 
-	public void setRadiusX(int radiusX) throws IllegalArgumentException {
-		if(radiusX < 0) {
+	public void setRadiusX(double radiusX) throws IllegalArgumentException {
+		if (radiusX < 0.0) {
 			throw new IllegalArgumentException();
 		}
 		this.radiusX = radiusX;
 	}
 
-	public int getRadiusY() {
+	public double getRadiusY() {
 		return radiusY;
 	}
 
-	public void setRadiusY(int radiusY) throws IllegalArgumentException {
-		if(radiusY < 0) {
+	public void setRadiusY(double radiusY) throws IllegalArgumentException {
+		if (radiusY < 0.0) {
 			throw new IllegalArgumentException();
 		}
 		this.radiusY = radiusY;
@@ -42,12 +42,18 @@ public class Ellipse extends Shape{
 	@Override
 	public String toString() {
 		String str = this.getType().toString() + " |";
-		if(this.getType() == ShapeType.Ellipse) {
+		if (this.getType() == ShapeType.Ellipse) {
 			str += " Radius X: " + this.getRadiusX() + " Radius Y: " + this.getRadiusY();
-		} else if(this.getType() == ShapeType.Circle) {
+		} else if (this.getType() == ShapeType.Circle) {
 			str += " Radius: " + this.getRadiusX();
 		}
 		str += super.toString();
 		return str;
+	}
+
+	@Override
+	public boolean isInside(Point p) {
+		return (Math.pow((p.getX() - this.getCenter().getX()), 2) * Math.pow(this.getRadiusX(), 2)) + ((Math.pow((p.getY() - this.getCenter().getY()), 2)) * (Math.pow(this.getRadiusY(), 2)))
+				<= (Math.pow(this.getRadiusX(), 2) * Math.pow(this.getRadiusY(), 2));
 	}
 }
